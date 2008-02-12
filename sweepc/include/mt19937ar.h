7a40ad9a691e09d23694ef092abb69a97ce911fe
Matthew Celnik <msc37@cam.ac.uk>
2008-02-12 17:26:21 +0000

    mt19937ar.h: Changed some #define parameters to prevent clashes with common variable names.

--- sweepc/include/mt19937ar.h
+++ sweepc/include/mt19937ar.h
@@ -42,20 +42,20 @@
 */
 
 /* Period parameters */  
-#define N 624
-#define M 397
+#define MTN 624
+#define MTM 397
 #define MATRIX_A 0x9908b0dfUL   /* constant vector a */
 #define UPPER_MASK 0x80000000UL /* most significant w-r bits */
 #define LOWER_MASK 0x7fffffffUL /* least significant r bits */
 
-static unsigned long mt[N]; /* the array for the state vector  */
-static int mti=N+1; /* mti==N+1 means mt[N] is not initialized */
+static unsigned long mt[MTN]; /* the array for the state vector  */
+static int mti=MTN+1; /* mti==N+1 means mt[N] is not initialized */
 
 /* initializes mt[N] with a seed */
 inline void init_genrand(unsigned long s)
 {
     mt[0]= s & 0xffffffffUL;
-    for (mti=1; mti<N; mti++) {
+    for (mti=1; mti<MTN; mti++) {
         mt[mti] = 
 	    (1812433253UL * (mt[mti-1] ^ (mt[mti-1] >> 30)) + mti); 
         /* See Knuth TAOCP Vol2. 3rd Ed. P.106 for multiplier. */
@@ -76,21 +76,21 @@ inline void init_by_array(unsigned long init_key[], int key_length)
     int i, j, k;
     init_genrand(19650218UL);
     i=1; j=0;
-    k = (N>key_length ? N : key_length);
+    k = (MTN>key_length ? MTN : key_length);
     for (; k; k--) {
         mt[i] = (mt[i] ^ ((mt[i-1] ^ (mt[i-1] >> 30)) * 1664525UL))
           + init_key[j] + j; /* non linear */
         mt[i] &= 0xffffffffUL; /* for WORDSIZE > 32 machines */
         i++; j++;
-        if (i>=N) { mt[0] = mt[N-1]; i=1; }
+        if (i>=MTN) { mt[0] = mt[MTN-1]; i=1; }
         if (j>=key_length) j=0;
     }
-    for (k=N-1; k; k--) {
+    for (k=MTN-1; k; k--) {
         mt[i] = (mt[i] ^ ((mt[i-1] ^ (mt[i-1] >> 30)) * 1566083941UL))
           - i; /* non linear */
         mt[i] &= 0xffffffffUL; /* for WORDSIZE > 32 machines */
         i++;
-        if (i>=N) { mt[0] = mt[N-1]; i=1; }
+        if (i>=MTN) { mt[0] = mt[MTN-1]; i=1; }
     }
 
     mt[0] = 0x80000000UL; /* MSB is 1; assuring non-zero initial array */ 
@@ -103,22 +103,22 @@ inline unsigned long genrand_int32(void)
     static unsigned long mag01[2]={0x0UL, MATRIX_A};
     /* mag01[x] = x * MATRIX_A  for x=0,1 */
 
-    if (mti >= N) { /* generate N words at one time */
+    if (mti >= MTN) { /* generate N words at one time */
         int kk;
 
-        if (mti == N+1)   /* if init_genrand() has not been called, */
+        if (mti == MTN+1)   /* if init_genrand() has not been called, */
             init_genrand(5489UL); /* a default initial seed is used */
 
-        for (kk=0;kk<N-M;kk++) {
+        for (kk=0;kk<MTN-MTM;kk++) {
             y = (mt[kk]&UPPER_MASK)|(mt[kk+1]&LOWER_MASK);
-            mt[kk] = mt[kk+M] ^ (y >> 1) ^ mag01[y & 0x1UL];
+            mt[kk] = mt[kk+MTM] ^ (y >> 1) ^ mag01[y & 0x1UL];
         }
-        for (;kk<N-1;kk++) {
+        for (;kk<MTN-1;kk++) {
             y = (mt[kk]&UPPER_MASK)|(mt[kk+1]&LOWER_MASK);
-            mt[kk] = mt[kk+(M-N)] ^ (y >> 1) ^ mag01[y & 0x1UL];
+            mt[kk] = mt[kk+(MTM-MTN)] ^ (y >> 1) ^ mag01[y & 0x1UL];
         }
-        y = (mt[N-1]&UPPER_MASK)|(mt[0]&LOWER_MASK);
-        mt[N-1] = mt[M-1] ^ (y >> 1) ^ mag01[y & 0x1UL];
+        y = (mt[MTN-1]&UPPER_MASK)|(mt[0]&LOWER_MASK);
+        mt[MTN-1] = mt[MTM-1] ^ (y >> 1) ^ mag01[y & 0x1UL];
 
         mti = 0;
     }
