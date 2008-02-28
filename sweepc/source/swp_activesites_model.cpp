f3b9b69c6543fdc34821aa6b9645e62e2fe4d157
Matthew Celnik <msc37@cam.ac.uk>
2008-02-28 12:22:40 +0000

    Added swp_activesites_model.cpp.

---
+++ sweepc/source/swp_activesites_model.cpp
@@ -0,0 +1,21 @@
+#include "swp_activesites_model.h"
+
+using namespace Sweep;
+
+ActiveSitesModel::ActiveSitesModel(void)
+{
+}
+
+ActiveSitesModel::ActiveSitesModel(const ActiveSitesModel &copy)
+{
+    *this = copy;
+}
+
+ActiveSitesModel::~ActiveSitesModel(void)
+{
+}
+
+ActiveSitesModel &ActiveSitesModel::operator=(const ActiveSitesModel &rhs)
+{
+    return *this;
+}
