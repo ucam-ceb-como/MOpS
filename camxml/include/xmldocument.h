44c04dd4a3a7d67559ae2b237658020400486406
Matthew Celnik <msc37@cam.ac.uk>
2008-07-02 14:52:30 +0000

    Added copyright notice, licence notice and contact details to the top of every file.

--- camxml/include/xmldocument.h
+++ camxml/include/xmldocument.h
@@ -1,9 +1,42 @@
 /*
   Author(s):      Matthew Celnik (msc37)
   Project:        camxml (Cambridge c++ XML library)
+  Sourceforge:    http://sourceforge.net/projects/mopssuite
+  
+  Copyright (C) 2008 Matthew S Celnik.
 
   File purpose:
     Defines an XML document.
+
+  Licence:
+    This file is part of the "camxml" library.
+
+    camxml is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public License
+    as published by the Free Software Foundation; either version 2
+    of the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+  Contact:
+    Dr Markus Kraft
+    Dept of Chemical Engineering
+    University of Cambridge
+    New Museums Site
+    Pembroke Street
+    Cambridge
+    CB2 3RA
+    UK
+
+    Email:       mk306@cam.ac.uk
+    Website:     http://como.cheng.cam.ac.uk
 */
 
 #ifndef CAMXML_DOCUMENT_H
