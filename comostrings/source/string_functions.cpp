63327e10daf1424f4928b37a0b1ac0e9b60cd27a
Matthew Celnik <msc37@cam.ac.uk>
2008-07-02 14:41:39 +0000

    Added copyright notice, licence notice and contact details to the top of every file.

--- comostrings/source/string_functions.cpp
+++ comostrings/source/string_functions.cpp
@@ -1,3 +1,46 @@
+/*
+
+  Author(s):      Matthew Celnik (msc37)
+  Project:        comostrings (mopssuite)
+  Sourceforge:    http://sourceforge.net/projects/mopssuite
+  
+  Copyright (C) 2008 Matthew S Celnik.
+
+  File purpose:
+    Implementation of the functions declared in the string_functions.h
+    header file.
+
+  Licence:
+    This file is part of the "comostrings" library.
+
+    comostrings is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public License
+    as published by the Free Software Foundation; either version 2
+    of the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+  Contact:
+    Dr Markus Kraft
+    Dept of Chemical Engineering
+    University of Cambridge
+    New Museums Site
+    Pembroke Street
+    Cambridge
+    CB2 3RA
+    UK
+
+    Email:       mk306@cam.ac.uk
+    Website:     http://como.cheng.cam.ac.uk
+*/
+
 #include "string_functions.h"
 #include <string>
 #include <vector>
