b80780f72aa7cb658391870c342c0e7b81d378f3
Matthew Celnik <msc37@cam.ac.uk>
2008-07-02 16:20:30 +0000

    Added copyright notice, licence notice and contact information to all files.

--- sprogc/include/gpc_gasphase.h
+++ sprogc/include/gpc_gasphase.h
@@ -1,6 +1,9 @@
 /*
   Author(s):      Matthew Celnik (msc37)
   Project:        sprog (gas-phase chemical kinetics).
+  Sourceforge:    http://sourceforge.net/projects/mopssuite
+  
+  Copyright (C) 2008 Matthew S Celnik.
 
   File purpose:
     The GasPhase class is a child of the Mixture class that also has thermodynamic
@@ -16,6 +19,36 @@
     way, speed improvements can be gained if the mixture is unchanged between
     calls to calculate the properties.  If the mixture is changed then the 
     properties are recalculated the next time the relevant routines are called.
+
+  Licence:
+    This file is part of "sprog".
+
+    sprog is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public License
+    as published by the Free Software Foundation; either version 2
+    of the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+  Contact:
+    Dr Markus Kraft
+    Dept of Chemical Engineering
+    University of Cambridge
+    New Museums Site
+    Pembroke Street
+    Cambridge
+    CB2 3RA
+    UK
+
+    Email:       mk306@cam.ac.uk
+    Website:     http://como.cheng.cam.ac.uk
 */
 
 #ifndef GPC_GASPHASE_H
--- sprogc/include/gpc_mech.h
+++ sprogc/include/gpc_mech.h
@@ -1,12 +1,45 @@
 /*
   Author(s):      Matthew Celnik (msc37)
   Project:        sprog (gas-phase chemical kinetics).
+  Sourceforge:    http://sourceforge.net/projects/mopssuite
+  
+  Copyright (C) 2008 Matthew S Celnik.
 
   File purpose:
     This files contains the definition of a chemical mechanism.  A chemical mechanism consists
     of 3 parts:  definition of chemical elements, definition of chemical species and a set of
     reactions which define the systems kinetics.  By combining these items into a single
     class the code can be made more efficient.
+
+  Licence:
+    This file is part of "sprog".
+
+    sprog is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public License
+    as published by the Free Software Foundation; either version 2
+    of the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+  Contact:
+    Dr Markus Kraft
+    Dept of Chemical Engineering
+    University of Cambridge
+    New Museums Site
+    Pembroke Street
+    Cambridge
+    CB2 3RA
+    UK
+
+    Email:       mk306@cam.ac.uk
+    Website:     http://como.cheng.cam.ac.uk
 */
 
 #ifndef GPC_MECH_H
