b5f5a819daf245b5da84f17f2c776325248f793f
Markus Sander <ms785@cam.ac.uk>
2008-11-20 18:24:30 +0000

    added EnsembleImage class to sweep

---
+++ sweepc/source/swp_ensemble_image.cpp
@@ -0,0 +1,381 @@
+/*
+  Author(s):      Matthew Celnik (msc37) and Markus Sander (ms785)
+  Project:        sweepc (population balance solver)
+  Sourceforge:    http://sourceforge.net/projects/mopssuite
+  
+  Copyright (C) 2008 Matthew S Celnik and Markus Sander.
+
+  File purpose:
+    Implementation of the EnsembleImage class declared in the
+    swp_ensemble_image.h header file.
+
+  Licence:
+    This file is part of "sweepc".
+
+    sweepc is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public License
+    as published by the Free Software Foundation; either version 2
+    of the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+  Contact:
+    Dr Markus Kraft
+    Dept of Chemical Engineering
+    University of Cambridge
+    New Museums Site
+    Pembroke Street
+    Cambridge
+    CB2 3RA
+    UK
+
+    Email:       mk306@cam.ac.uk
+    Website:     http://como.cheng.cam.ac.uk
+*/
+
+#include "swp_ensemble_image.h"
+#include "swp_particle_image.h"
+#include "swp_imgnode.h"
+#include "rng.h"
+#include "string_functions.h"
+#include <fstream>
+#include <vector>
+#include <stdexcept>
+#include "swp_mechanism.h"
+using namespace Sweep;
+using namespace std;
+using namespace Strings;
+using namespace Sweep::Imaging;
+
+
+
+// Default contructor.
+EnsembleImage::EnsembleImage(void)
+{
+}
+
+
+// Destructor.
+EnsembleImage::~EnsembleImage(void)
+{
+}
+/*
+void EnsembleImage::PrintEnsemble(Cell &sys,std::ofstream &file)
+{	Ensemble::iterator i;
+    ParticleCache::PropID id=ParticleCache::iD;
+	m_root.Clear();
+	for (i=sys.Particles().begin(); i!=sys.Particles().end(); ++i) {
+		if ((*(*i)).Property(id)>5e-9)
+		m_root.Insert((*(*i)).Property(id),(*i));						//add 1e-8 to the radius to let the particles not collide
+	}
+    calc_FM_ensemble(m_root);
+    m_root.CentreCOM();
+	Write3dout(file,&m_root);
+	
+}
+
+
+void EnsembleImage::Write3dout(std::ofstream &file, EnsembleImgNode *curr_node)
+{
+    if (file.good()) {
+        string line;
+		if (curr_node->IsLeaf() && curr_node->sp!=NULL) 
+		{ 		
+			Sweep::Imaging::ParticleImage *img= new Sweep::Imaging::ParticleImage;
+			img->Construct(*curr_node->sp);
+			img->Write3dout(file,1e9*curr_node->m_cen_mass[0],1e9*curr_node->m_cen_mass[1],1e9*curr_node->m_cen_mass[2]);
+			delete img;
+		}
+		else 
+		{
+			Write3dout(file, curr_node->m_left);
+			Write3dout(file, curr_node->m_right);
+		}
+
+
+    } else {
+        throw invalid_argument("Output stream not ready "
+                               "(Sweep, EnsembleImage::Write3dout).");
+    }
+}
+*/
+
+
+
+void EnsembleImage::PrintEnsemble(Cell &sys,std::ofstream &file)
+{
+	
+	bool treedimage=false;
+	Ensemble::iterator i;
+    ParticleCache::PropID id=ParticleCache::iV;
+	double x,y,z;
+	double maxd=0;
+	double boxlength;
+	int numsubpart=0;
+	x=0;y=0;z=0;
+	m_root.Clear();
+	for (i=sys.Particles().begin(); i!=sys.Particles().end(); ++i) {
+		maxd=max((*(*i)).Property(id),maxd);
+		numsubpart++;
+		if (numsubpart>100) break;
+	}
+	if (treedimage==true)
+	{
+		maxd=1e9*6*pow(maxd,0.3333333);
+		boxlength=pow(numsubpart,0.33333333);
+		boxlength*=maxd;
+	}
+	else 
+	{
+		maxd=1e9*6*pow(maxd,0.3333333);
+		boxlength=pow(numsubpart,0.5);
+		boxlength*=maxd;
+	}
+
+
+	numsubpart=0;
+	for (i=sys.Particles().begin(); i!=sys.Particles().end(); ++i) {
+		numsubpart++;
+		if(numsubpart<100)
+		{ 
+			if (x<boxlength && y<boxlength)
+			{
+				x=x+maxd; 		
+			}
+			if (x>boxlength) 
+			{
+				x=0;
+				y=y+maxd;
+			}
+			if (y>boxlength) 
+			{
+				x=0;
+				y=0;
+				z=z+maxd;
+			}
+			Sweep::Imaging::ParticleImage img;
+			img.Construct(*(*i));
+			img.Write3dout(file,x+(rnd()-0.5)*(boxlength-(*(*i)).Property(id)*1e9),y+(rnd()-0.5)*(boxlength-(*(*i)).Property(id)*1e9),z+(rnd()-0.5)*(boxlength-(*(*i)).Property(id)*1e9));
+		}
+	}
+
+	
+}
+
+
+
+
+// Calculates the aggregate structure down from the given
+// node.  Assumes that the tree leaves have been initialised
+// with the correct radii, and recalculates their positions.
+void EnsembleImage::calc_FM_ensemble(EnsembleImgNode &node)
+{
+    EnsembleImgNode *target = node.m_left;
+    EnsembleImgNode *bullet = node.m_right;
+
+    if ((target != NULL) && (bullet != NULL)) {
+        // Pass calculation down binary tree left & right branches.
+        calc_FM_ensemble(*target);
+        calc_FM_ensemble(*bullet);
+
+        // The first part of the collision algorithm is to
+        // randomly orientate both left and right aggregates.
+        // They are both then placed so that their bounding
+        // spheres are at the origin.
+
+        // Rotate left node randomly about CoM.
+        real phi1   = rnd() * 2.0 * PI;
+        real theta1 = ((2.0*rnd())-1.0) * PI;
+        target->RotateCOM(theta1, phi1);
+        
+        // Rotate right node randomly about CoM.
+        real phi2   = rnd() * 2.0 * PI;
+        real theta2 = ((2.0*rnd())-1.0) * PI;
+        bullet->RotateCOM(theta2, phi2);
+
+        // Move both spheres so that the bounding spheres
+        // sit at the origin.
+        target->CentreBoundSph();
+        bullet->CentreBoundSph();
+
+        // Perform the collision of the left and right nodes.
+        // This ma require several iterations if the chosen
+        // x-y displacement means that the aggregates cannot
+        // collide in the z-direction.
+        Coords::Vector D;
+        real sumr=0.0;
+        bool hit = false;
+        while (!hit) {
+            // Need to reset target and bullet here, in case
+            // the have been changed by the tree traversal
+            // code below.
+            target = node.m_left;
+            bullet = node.m_right;
+            sumr = target->Radius() + bullet->Radius();
+
+            // Create a random displacement of the bullet node
+            // in the x-y plane.  The displacement is never
+            // greater than the sum of the radii, therefore they
+            // should always touch.
+            D[0] = ((2.0 * rnd()) - 1.0) * sumr;
+            D[1] = ((2.0 * rnd()) - 1.0) * sumr;
+
+            //// Calculate the z-position for the collision of the
+            //// target and bullet.  We do this in case both the
+            //// target and bullet are leaf nodes, and hence the
+            //// binary tree traversal won't happen.
+           // hit = calcCollZ(target->m_cen_mass, target->m_r,
+           //                 bullet->m_cen_mass, bullet->m_r,
+           //                 D[0], D[1], dz1);
+           // if (!hit) continue; // Should never happen.
+           // D[2] = target->m_cen_bsph[2] + dz1;
+
+            // The next code determines the displacement along the z-axis
+            // required for the target and bullet aggregates to touch.  This
+            // requires falling down the tree progressively recalculating
+            // the nearest nodes at each level, until the leaf nodes
+            // are reached.
+            // This next code calculates the minimum distance between the
+            // target's children and bullet's children, or the target or
+            // bullet if they have no children.  The two children with
+            // the smallest separation are chosen as the next target
+            // and bullet.
+            hit = minCollZ_ensemble(*target, *bullet, D[0], D[1], D[2]);
+        }
+
+        // We have a new location for the bullet (right node), so move it.
+        node.m_right->Translate(D[0], D[1], D[2]);
+
+        // Calculate properties of this node.
+        node.CalcBoundSph();
+        node.CalcCOM();
+        node.CentreBoundSph();
+    }
+}
+
+// Calculates the minimum collision distance between
+// a target and a bullet node by moving down the
+// binary tree.  If the nodes collide then returns
+// true, otherwise returns false.
+bool EnsembleImage::minCollZ_ensemble(const EnsembleImgNode &target, 
+                             const EnsembleImgNode &bullet, 
+                             real dx, real dy, real &dz)
+{
+    bool hit=false, hit1=false;
+    real dz2=0.0, dz3=0.0, dz4=0.0;
+
+    if (target.IsLeaf()) {
+        // Target is a leaf
+        if (bullet.IsLeaf()) {
+            // Bullet is a leaf (both leaves).
+           return calcCollZ_ensemble(target.BoundSphCentre(), target.Radius(),
+                            bullet.BoundSphCentre(), bullet.Radius(), 
+                            dx, dy, dz);
+        } else {
+            // Bullet is not a leaf, call sub-nodes.
+            // Calculate minimum dz for the target and the bullet left subnode.
+            hit1 = calcCollZ_ensemble(target.BoundSphCentre(), target.Radius(),
+                             bullet.m_left->BoundSphCentre(), bullet.m_left->Radius(), 
+                             dx, dy, dz);
+            if (hit1) hit = minCollZ_ensemble(target, *bullet.m_left, dx, dy, dz);
+            // Calculate minimum dz for the target and the bullet right subnode.
+            hit1 = calcCollZ_ensemble(target.BoundSphCentre(), target.Radius(),
+                             bullet.m_right->BoundSphCentre(), bullet.m_right->Radius(), 
+                             dx, dy, dz2);
+            if (hit1) hit = minCollZ_ensemble(target, *bullet.m_right, dx, dy, dz2) || hit;
+            // Return minimum dz.
+            dz = min(dz, dz2);
+            return hit;
+        }
+    } else {
+        // Target is not a leaf.
+        if (bullet.IsLeaf()) {
+            // Bullet is a leaf, call target sub-nodes..
+            // Calculate minimum dz for the target left subnode and the bullet.
+            hit1 = calcCollZ_ensemble(target.m_left->BoundSphCentre(), target.m_left->Radius(),
+                             bullet.BoundSphCentre(), bullet.Radius(), 
+                             dx, dy, dz);
+            if (hit1) hit = minCollZ_ensemble(*target.m_left, bullet, dx, dy, dz);
+            // Calculate minimum dz for the target right subnode and the bullet.
+            hit1 = calcCollZ_ensemble(target.m_right->BoundSphCentre(), target.m_right->Radius(),
+                             bullet.BoundSphCentre(), bullet.Radius(), 
+                             dx, dy, dz2);
+            if (hit1) hit = minCollZ_ensemble(*target.m_right, bullet, dx, dy, dz2) || hit;
+            // Return minimum dz.
+            dz = min(dz, dz2);
+            return hit;
+        } else {
+            // Bullet is not a leaf (neither is a leaf), check all left/right
+            // collision combinations.
+            // Target left and bullet left.
+            hit1 = calcCollZ_ensemble(target.m_left->BoundSphCentre(), target.m_left->Radius(),
+                             bullet.m_left->BoundSphCentre(), bullet.m_left->Radius(), 
+                             dx, dy, dz);
+            if (hit1) hit = minCollZ_ensemble(*target.m_left, *bullet.m_left, dx, dy, dz);
+            // Target left and bullet right.
+            hit1 = calcCollZ_ensemble(target.m_left->BoundSphCentre(), target.m_left->Radius(),
+                             bullet.m_right->BoundSphCentre(), bullet.m_right->Radius(), 
+                             dx, dy, dz2);
+            if (hit1) hit = minCollZ_ensemble(*target.m_left, *bullet.m_right, dx, dy, dz2) || hit;
+            // Target right and bullet left.
+            hit1 = calcCollZ_ensemble(target.m_right->BoundSphCentre(), target.m_right->Radius(),
+                             bullet.m_left->BoundSphCentre(), bullet.m_left->Radius(), 
+                             dx, dy, dz3);
+            if (hit1) hit = minCollZ_ensemble(*target.m_right, *bullet.m_left, dx, dy, dz3) || hit;
+            // Target right and bullet right.
+            hit1 = calcCollZ_ensemble(target.m_right->BoundSphCentre(), target.m_right->Radius(),
+                             bullet.m_right->BoundSphCentre(), bullet.m_right->Radius(), 
+                             dx, dy, dz4);
+            if (hit1) hit = minCollZ_ensemble(*target.m_right, *bullet.m_right, dx, dy, dz4) || hit;
+            // Returns minimum dz.
+            dz = min(min(dz, dz2), min(dz3, dz4));
+            return hit;
+        }
+    }
+}
+
+// Calculates the z-displacement of a bullet sphere for a +ve
+// collision with a target sphere.  Returns true if the
+// spheres collide, otherwise false.
+bool EnsembleImage::calcCollZ_ensemble(const Coords::Vector &p1, real r1, 
+                              const Coords::Vector &p2, real r2, 
+                              real dx, real dy, real &dz)
+{
+    // Calculate the square of the sum of the radii.
+    real sumrsqr = r1 + r2; sumrsqr *= sumrsqr;
+
+    // Calculate dx, dy and dz.  Remember to include
+    // argument contributions.
+    real xdev = p2[0] - p1[0] + dx;
+    real ydev = p2[1] - p1[1] + dy;
+    real zdev = p2[2] - p1[2];
+
+    // Calculate dx, dy and dz squared.
+    real dxsqr = xdev * xdev;
+    real dysqr = ydev * ydev;
+    real dzsqr = zdev * zdev;
+
+    // Calculate quadratic terms.
+    real b = 2.0 * zdev;
+    real c = dxsqr + dysqr + dzsqr - sumrsqr;
+
+    // Calculate determinant.
+    real det = (b*b) - (4.0*c);
+
+    if (det >= 0.0) {
+        // Spheres intersect.
+        dz = - 0.5 * (b + sqrt(det));
+        return true;
+    } else {
+        // Spheres do not intersect.
+        dz = 1.0e10; // A large number.
+        return false;
+    }
+}
---
+++ sweepc/source/swp_ensemble_imgnode.cpp
@@ -0,0 +1,391 @@
+/*
+  Author(s):      Matthew Celnik (msc37) and Markus Sander (ms785)
+  Project:        sweepc (population balance solver)
+  Sourceforge:    http://sourceforge.net/projects/mopssuite
+  
+  Copyright (C) 2008 Matthew S Celnik and Markus Sander.
+
+  File purpose:
+    Implementation of the ImgNode class declared in the
+    swp_imgnode.h header file.
+
+  Licence:
+    This file is part of "sweepc".
+
+    sweepc is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public License
+    as published by the Free Software Foundation; either version 2
+    of the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+  Contact:
+    Dr Markus Kraft
+    Dept of Chemical Engineering
+    University of Cambridge
+    New Museums Site
+    Pembroke Street
+    Cambridge
+    CB2 3RA
+    UK
+
+    Email:       mk306@cam.ac.uk
+    Website:     http://como.cheng.cam.ac.uk
+*/
+
+#include "swp_ensemble_imgnode.h"
+#include "swp_subparticle.h"
+
+
+using namespace Sweep;
+using namespace std;
+using namespace Sweep::Imaging;
+
+// CONSTRUCTORS AND DESTRUCTORS.
+
+// Default constructor.
+EnsembleImgNode::EnsembleImgNode(void)
+: m_r(0.0), m_r2(0.0), m_r3(0.0), m_mindepth(0), m_parent(NULL), 
+  m_left(NULL), m_right(NULL)
+{
+}
+
+// Destructor.
+EnsembleImgNode::~EnsembleImgNode(void)
+{
+    delete m_left;
+    delete m_right;
+}
+
+
+// TREE BUILDING (PRIMARY ADDITION).
+
+// Clears the node back to initial state.  Also clears
+// children.
+void EnsembleImgNode::Clear(void)
+{
+    // Set size to zero.
+    m_r  = 0.0;
+    m_r2 = 0.0;
+    m_r3 = 0.0;
+
+    // Centre coordinates.
+    m_cen_bsph[0] = 0.0;
+    m_cen_bsph[1] = 0.0;
+    m_cen_bsph[2] = 0.0;
+    m_cen_mass[0] = 0.0;
+    m_cen_mass[1] = 0.0;
+    m_cen_mass[2] = 0.0;
+
+    // Clear children.
+    if (m_left != NULL) {
+        m_left->Clear();
+        delete m_left; m_left = NULL;
+    }
+    if (m_right != NULL) {
+        m_right->Clear();
+        delete m_right; m_right = NULL;
+    }
+}
+
+
+
+void EnsembleImgNode::CopySPT(const SubParticle *sp)
+{
+	Primary::PropID id=Primary::iD;
+
+    if (sp->Primary()== NULL) {
+
+            m_left = new EnsembleImgNode();
+            m_left->m_parent = this;
+			m_left->CopySPT(sp->Left());
+
+            m_right = new EnsembleImgNode();
+            m_right->m_parent = this;
+			m_right->CopySPT(sp->Right());
+
+
+           setRadius(0.0);
+
+
+           m_mindepth = 1;
+        
+    } else {
+        
+			setRadius( (sp->Primary()->Property(id))*0.5e9);       //convert to nm
+
+       
+    }
+}
+
+
+// Adds a primary into the tree.  Algorithm maintains
+// well-balanced binary tree structure by observing
+// the minimum branch depth below node.
+void EnsembleImgNode::Insert(real radius,Particle *sp)
+{
+    // Check for an empty tree.
+    if ((m_left == NULL) && (m_right == NULL)) {
+        if (m_r == 0.0) {
+            // This node is empty, just set its radius.
+            setRadius(radius);
+			this->sp=sp;
+            m_mindepth = 0;
+        } else {
+            // This node is not empty, but also has
+            // no children.
+
+            // Set left child to have the properties
+            // of this node.
+            m_left = new EnsembleImgNode();
+            m_left->setRadius(m_r);
+			m_left->sp=this->sp;
+            m_left->m_parent = this;
+
+            // Add a new right node for the new primary.
+            m_right = new EnsembleImgNode();
+            m_right->setRadius(radius);
+			m_right->sp=sp;
+            m_right->m_parent = this;
+
+            // Mark this node as not calculated.
+           setRadius(0.0);
+		   this->sp=NULL;
+
+           // Set the tree depth.
+           m_mindepth = 1;
+        }
+    } else {
+        // This node has children.
+        if (m_left->m_mindepth <= m_right->m_mindepth) {
+            // Add primary to the left branch if branch
+            // depths are equal, or left branch is shallower.
+            m_left->Insert(radius,sp);
+        } else {
+            // Add primary to the right branch if it is
+            // shallower.
+            m_right->Insert(radius,sp);
+        }
+        // Recalulate the minimum tree depth of this node.
+        m_mindepth = min(m_left->m_mindepth, m_right->m_mindepth)+1;
+    }
+}
+
+// Returns true if this node is a leaf (has no children).
+bool EnsembleImgNode::IsLeaf(void) const
+{
+    return (m_left==NULL) && (m_right==NULL);
+}
+
+// Returns a vector of primary coordinates (4D).  The first
+// three returned values are the cartesian x,y,z coordinates,
+// the final value is the radius.
+void EnsembleImgNode::GetPriCoords(std::vector<fvector> &coords) const
+{
+    if (IsLeaf()) {
+        fvector c(4);
+        c[0] = m_cen_mass[0];
+        c[1] = m_cen_mass[1];
+        c[2] = m_cen_mass[2];
+        c[3] = m_r;
+        coords.push_back(c);
+    } else {
+        m_left->GetPriCoords(coords);
+        m_right->GetPriCoords(coords);
+    }
+}
+
+
+// COORDINATE MANIPULATION.
+
+// Translates (moves) the aggregate node and child structure
+// by the given amounts along the cartesian axes.
+void EnsembleImgNode::Translate(real dx, real dy, real dz)
+{
+    // Translate child branches.
+    if (m_left != NULL) m_left->Translate(dx, dy, dz);
+    if (m_right != NULL) m_right->Translate(dx, dy, dz);
+    // Translate bounding sphere and centre-of-mass centres.
+    m_cen_bsph.Translate(dx, dy, dz);
+    m_cen_mass.Translate(dx, dy, dz);
+}
+
+// Put the centre-of-mass at the origin.
+void EnsembleImgNode::CentreCOM(void)
+{
+    Translate(-m_cen_mass[0], -m_cen_mass[1], -m_cen_mass[2]);
+}
+
+// Put the bounding-sphere at the origin.
+void EnsembleImgNode::CentreBoundSph(void)
+{
+    Translate(-m_cen_bsph[0], -m_cen_bsph[1], -m_cen_bsph[2]);
+}
+
+// Rotates the aggregate node and child structure about its centre
+// of mass by the given angles (spherical coordinates).
+void EnsembleImgNode::RotateCOM(real dtheta, real dphi)
+{
+    // Move the aggregate so that its centre-of-mass
+    // is at the origin.  Store the coordinates, so that
+    // they can be restored afterwards.
+    Coords::Vector D(m_cen_mass);
+    Translate(-D.X(), -D.Y(), -D.Z());
+
+    // Create transformation matrix.
+    Coords::Matrix M;
+    M.SetIdentity();
+    M.Rotate(dtheta, dphi);
+
+    // Rotate child nodes.
+    if (m_left != NULL) m_left->transform(M);
+    if (m_right != NULL) m_right->transform(M);
+
+    // Rotate bounding-sphere coordinates.
+    m_cen_bsph = M.Mult(m_cen_bsph);
+
+    // Restore centre-of-mass coordinates.
+    Translate(D.X(), D.Y(), D.Z());
+}
+
+// Rotates the aggregate node and child structure about its bounding
+// sphere centre by the given angles (spherical coordinates).
+void EnsembleImgNode::RotateBoundSph(real dtheta, real dphi)
+{
+    // Move the aggregate so that its bounding sphere
+    // is at the origin.  Store the coordinates, so that
+    // they can be restored afterwards.
+    Coords::Vector D(m_cen_bsph);
+    Translate(-D.X(), -D.Y(), -D.Z());
+
+    // Create transformation matrix.
+    Coords::Matrix M;
+    M.SetIdentity();
+    M.Rotate(dtheta, dphi);
+
+    // Rotate child nodes.
+    if (m_left != NULL) m_left->transform(M);
+    if (m_right != NULL) m_right->transform(M);
+
+    // Rotate centre-of-mass coordinates.
+    m_cen_mass = M.Mult(m_cen_mass);
+
+    // Restore bounding-sphere coordinates.
+    Translate(D.X(), D.Y(), D.Z());
+}
+
+// Rotates the aggregate node and child structure about the
+// coordinate system origin by the given angles (spherical coordinates).
+void EnsembleImgNode::RotateOrigin(real dtheta, real dphi)
+{
+    // Create transformation matrix.
+    Coords::Matrix M;
+    M.SetIdentity();
+    M.Rotate(dtheta, dphi);
+    // Perform transformation.
+    transform(M);
+}
+
+// Transforms the node coordinates using the given
+// transformation matrix.
+void EnsembleImgNode::transform(const Coords::Matrix &mat)
+{
+    // Rotate child nodes.
+    if (m_left != NULL) m_left->transform(mat);
+    if (m_right != NULL) m_right->transform(mat);
+    // Rotate centre-of-mass and bounding sphere coords.
+    m_cen_mass = mat.Mult(m_cen_mass);
+    m_cen_bsph = mat.Mult(m_cen_bsph);
+}
+
+
+// BOUNDING SPHERE.
+
+// Returns the bounding-sphere centre.
+const Coords::Vector &EnsembleImgNode::BoundSphCentre(void) const
+{
+    return m_cen_bsph;
+}
+
+// Returns the bounding sphere radius.
+real EnsembleImgNode::Radius(void) const
+{
+    return m_r;
+}
+
+// Sets the radius of the bounding sphere.
+void EnsembleImgNode::setRadius(real r)
+{
+    m_r  = r;
+    m_r2 = r * r;
+    m_r3 = m_r2 * m_r;
+}
+
+// Calculates the bounding sphere position and radius using
+// the left and right child node values.
+void EnsembleImgNode::CalcBoundSph(void)
+{
+    if ((m_left != NULL) && (m_right != NULL)) {
+        // Calculate bounding spheres of children.
+        m_left->CalcBoundSph();
+        m_right->CalcBoundSph();
+
+        // Calculate translation between left and right spheres.
+        real dx = m_right->m_cen_bsph[0] - m_left->m_cen_bsph[0];
+        real dy = m_right->m_cen_bsph[1] - m_left->m_cen_bsph[1];
+        real dz = m_right->m_cen_bsph[2] - m_left->m_cen_bsph[2];
+
+        // Calculate bounding sphere centre.
+        m_cen_bsph[0] = m_left->m_cen_bsph[0] + (0.5 * dx);
+        m_cen_bsph[1] = m_left->m_cen_bsph[1] + (0.5 * dy);
+        m_cen_bsph[2] = m_left->m_cen_bsph[2] + (0.5 * dz);
+
+        // Calculate bounding sphere radius.
+        setRadius(sqrt((dx*dx)+(dy*dy)+(dz*dz)));
+    }
+}
+
+
+// CENTRE-OF-MASS.
+
+// Returns the centre-of-mass.
+const Coords::Vector &EnsembleImgNode::CentreOfMass(void) const
+{
+    return m_cen_mass;
+}
+
+// Calculates the centre-of-mass using the left and right child
+// node values.
+void EnsembleImgNode::CalcCOM(void)
+{
+    if ((m_left != NULL) && (m_right != NULL)) {
+        // Calculate centres-of-mass of left and
+        // right children.
+        m_left->CalcCOM();
+        m_right->CalcCOM();
+
+        // Mass is proportional to r^3.  Calculated total
+        // mass (inverse) of left and right children.
+        real invtotmass = 1.0 / (m_left->m_r3 + m_right->m_r3);
+
+        // Now calculate centre of mass.
+        for (unsigned int i=0; i!=3; ++i) {
+            m_cen_mass[i]  = m_left->m_cen_mass[i] * m_left->m_r3;
+            m_cen_mass[i] += m_right->m_cen_mass[i] * m_right->m_r3;
+            m_cen_mass[i] *= invtotmass;
+        }
+    } else {
+        // If there are no children, then the centre-of-mass and
+        // bounding sphere centre are the same.
+        m_cen_mass[0] = m_cen_bsph[0];
+        m_cen_mass[1] = m_cen_bsph[1];
+        m_cen_mass[2] = m_cen_bsph[2];
+    }
+}
