5e50f8b50578820f70dacefd69875f5b0d173f03
Vinod Janardhanan <vj231@cam.ac.uk>
2009-01-05 12:07:08 +0000

    more constants added

--- sprogc/include/gpc_params.h
+++ sprogc/include/gpc_params.h
@@ -63,7 +63,12 @@ namespace Sprog
 
 
     // PHYSICAL CONSTANTS.
-
+	// These numbers are added by Vinod
+	const real kB	= 1.380658e-23; // Boltzmann constant
+	const real EPSILON0 = 8.854187816e-12;
+	// conversion factors
+	const real Debye__ = 3.33564e-30; // 
+	const real Angstroem__ = 1.0e-10;
     // Avogadro's number (source = NIST website, physics.nist.gov).
     // Error = 3.0e16 /mol.
     const real NA    = 6.02214179e23; // 1/mol.
@@ -71,15 +76,11 @@ namespace Sprog
     // Gas constant (source = NIST website, physics.nist.gov).
     // Error = 1.5e-5 J/mol/K.
     const real R     = 8.314472e0; // J/mol/K   (SI).
+	//const real R = kB*NA;
     const real R_CGS = 8.314472e7; // ergs/molK (CGS).
     const real RCAL  = 1.9872e-3;  // kcal/molK (calories).
 
-	// These numbers are added by Vinod
-	const real kB	= 1.380658e-23; // Boltzmann constant
-	const real EPSILON0 = 8.854187816e-12;
-	// conversion factors
-	const real Debye__ = 3.33564e-30; // 
-	const real Angstroem__ = 1.0e-10;
+
 };
 
 #endif
