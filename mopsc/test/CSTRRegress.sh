043b4e8368b38aba8c0b07549fd658df4db15dce
Rebecca Riehl <rcr33@cam.ac.uk>
2010-03-03 16:57:05 +0000

    Fixed CSTR regression test so that it works in Linux.

--- mopsc/test/CSTRRegress.sh
+++ mopsc/test/CSTRRegress.sh
@@ -1,8 +1,51 @@
 #!/bin/bash
 
+#  Copyright (C) 2010 Rebecca C Riehl.
+#
+#
+# Licence:
+#    This file is part of "mops".
+#
+#    brush is free software; you can redistribute it and/or
+#    modify it under the terms of the GNU General Public License
+#    as published by the Free Software Foundation; either version 2
+#    of the License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program; if not, write to the Free Software
+#    Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+#  Contact:
+#    Prof Markus Kraft
+#    Dept of Chemical Engineering
+#    University of Cambridge
+#    New Museums Site
+#    Pembroke Street
+#    Cambridge
+#    CB2 3RA
+#    UK
+#
+#    Email:       mk306@cam.ac.uk
+#    Website:     http://como.cheng.cam.ac.uk
+
 cd cstrtest
-# run mops on a very simple CSTR problem
-../../bin/mops_d.exe -gpc -p -diag4 -rr mops.inx -s CSTRRegress.xml -c chem.inp -t therm.dat
+
+#Choose the windows or linux names for the executable
+uname -s | grep --ignore-case CYGWIN 
+if(($?==0))
+then
+	program="../../bin/mops_d.exe"
+else 
+	program="../../bin/mops_d.x" 
+fi
+
+$program -gpc -p -diag4 -rr mops.inx -s sweep.xml -c chem.inp -t therm.dat
+
 if(($?==0)) 
 then
   echo "Finished simulation"
@@ -11,12 +54,6 @@ else
   exit $?
 fi
 echo "========================"
-
-# Check to see if the same outlet concentrations for 
-# Put in standard values for this type of reaction.
-# If the functionality has been changed, the CSTR will not 
-#give the same outputs.
-
 
 dos2unix "J3-chem.csv"
 dos2unix "J3-sensi.csv"
@@ -28,7 +65,7 @@ if(($?!=0))
 fi
 
 
-rm J3*
+rm -f J3*
 # All tests passed
 #echo "All tests passed"
 
