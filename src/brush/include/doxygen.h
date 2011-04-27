fa5002d983b5b1e5db61fe28e2a407574be6e83d
Robert Patterson <Robert.Patterson@wias-berlin.de>
2011-04-27 12:22:27 +0200

    Provide front page content for the doxygen documentation of sweep, mops and brush

---
+++ src/brush/include/doxygen.h
@@ -0,0 +1,38 @@
+/**
+  \file    doxygen.h
+  \author  Robert I A Patterson
+  \email   Robert.Patterson@wias-berlin.de
+  \brief   Front page for the brush library documentation.
+
+  This file should not contain source code and should not be included in any
+  file that does contain source code.
+*/
+
+/**
+ * \mainpage Brush
+ *
+ *
+ * \section Outline
+ * Brush is part of a larger suite of particle simulation software that
+ * has its origins in the research group of Markus Kraft at the
+ * University of Cambridge http://como.cheng.cam.ac.uk .
+ *
+ * Brush provides support for the simulation of spatially inhomogeneous particle reactors.
+ *
+ * \section GPL License
+
+    brush is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation; either version 2
+    of the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
---
+++ src/sweepc/include/doxygen.h
@@ -0,0 +1,45 @@
+/**
+  \file    doxygen.h
+  \author  Robert I A Patterson
+  \email   Robert.Patterson@wias-berlin.de
+  \brief   Front page for the sweep library documentation.
+
+  This file should not contain source code and should not be included in any
+  file that does contain source code.
+*/
+
+/**
+ * \mainpage The sweep particle simulation library
+ * Sweep is generally referred to as sweepc (to distinguish it from an earlier
+ * and now independent Fortran version).
+ *
+ * \section History
+ * Sweep is part of a larger suite of particle simulation software that
+ * has its origins in the research group of Markus Kraft at the
+ * University of Cambridge http://como.cheng.cam.ac.uk .
+ * An early Fortran code for the simulation of soot formation was developed
+ * by Michael Balthasar and Jasdeep Singh amongst others.  Robert Patterson
+ * wrote a completely new Fortran code with similar functionality, exploiting
+ * the analysis Singh and Balthasar had carried out as they developed from
+ * scratch.  Patterson named his new code Sweep after the friend of the
+ * cartoon character Sooty, since the purpose of the code was to simulate
+ * soot formation.  Matt Celnik developed Sweep into a library for use in larger
+ * calculations (Mops) and forked the C++ family of which this code is a part.
+ *
+ * \section GPL License
+
+    sweepc is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation; either version 2
+    of the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
