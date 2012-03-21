ea366b05cb68661a49f4656cf65693b9cdfb029b
William Menz <wjm34@cam.ac.uk>
2012-03-21 11:22:32 +0000

    Add some documentation.

--- src/sweepc/include/swp_bintree_serializer.h
+++ src/sweepc/include/swp_bintree_serializer.h
@@ -1,9 +1,48 @@
-/*
- * serializer.h
+/*!
+ * @file    swp_bintree_serializer.h
+ * @author  William J Menz
+ * @brief   Template for serialising particle binary trees (e.g. silica)
  *
- *  Created on: 20 Mar 2012
- *      Author: wjm34
- */
+ *   Author(s):      William J Menz
+ *   Project:        sweepc (population balance solver)
+ *   Copyright (C) 2012 William J Menz
+ *
+ *   File purpose:
+ *      Template for serialising particle binary trees (e.g. silica). Particle
+ *      models must add the BinTreeSerializer class as a friend in order
+ *      for the template class to have access to private data members.
+ *
+ *   Licence:
+ *      This file is part of "sweepc".
+ *
+ *      sweepc is free software; you can redistribute it and/or
+ *      modify it under the terms of the GNU Lesser General Public License
+ *      as published by the Free Software Foundation; either version 2
+ *      of the License, or (at your option) any later version.
+ *
+ *      This program is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU Lesser General Public License for more details.
+ *
+ *      You should have received a copy of the GNU Lesser General Public
+ *      License along with this program; if not, write to the Free Software
+ *      Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ *      02111-1307, USA.
+ *
+ *   Contact:
+ *      Prof Markus Kraft
+ *      Dept of Chemical Engineering
+ *      University of Cambridge
+ *      New Museums Site
+ *      Pembroke Street
+ *      Cambridge
+ *      CB2 3RA, UK
+ *
+ *      Email:       mk306@cam.ac.uk
+ *      Website:     http://como.cheng.cam.ac.uk
+*/
+
 
 #ifndef SWEEP_BINTREE_SERIALIZER_H
 #define SWEEP_BINTREE_SERIALIZER_H
