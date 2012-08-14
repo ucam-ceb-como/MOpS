40706a66c48b7ceae0daa435355122a496872197
Dongping Chen <dc516@cam.ac.uk>
2012-08-14 13:15:34 +0100

    fix problem in the PAHTEST3

--- test/mopsc/pahtest3/pahtest3-ens.pl
+++ test/mopsc/pahtest3/pahtest3-ens.pl
@@ -50,7 +50,7 @@ while(<$momentFile>) {
 
   # Look for a line that begin with a number and has the first entry (the time)
   # equal (upto a small tolerance) to 0.005
-  if(($fields[0] =~ /^\d+/) && (abs($fields[1] - 0.005) < 1e-6 )) {
+  if(($fields[0] =~ /^\d+/) && (abs($fields[1] - 0.054072) < 1e-6 )) {
       # Third field should be the zeroth moment
       $m0 = $fields[4];
       #print "4: $fields[4], ";
@@ -64,20 +64,20 @@ while(<$momentFile>) {
 
 # 20 repetitions
 # mean values and 99% confidence interval widths
-# m0 (1.787+-0.0994)e18 m^-3
-# fv (1.33+-0.0199)e-8 
+# m0 (1.741+-0.153)e18 m^-3
+# fv (1.3384+-0.0121)e-8 
 
 print "$m0, $m1\n";
-if(abs($m0 -  1.787e18) > 0.994e17) {
-  print "Simulated mean M0 was $m0, when  1.787e18m^-3 expected\n";
+if(abs($m0 -  1.741e18) > 1.53e17) {
+  print "Simulated mean M0 was $m0, when  1.741e18m^-3 expected\n";
   print "**************************\n";
   print "****** TEST FAILURE ******\n";
   print "**************************\n";
   exit 1;
 }
 
-if(abs($m1 - 1.33e-8) > 1.99e-10) {
-  print "Simulated mean Fv was $m1, when 1.33e-8 expected\n";
+if(abs($m1 - 1.3384e-8) > 1.21e-10) {
+  print "Simulated mean Fv was $m1, when 1.3384e-8 expected\n";
   print "**************************\n";
   print "****** TEST FAILURE ******\n";
   print "**************************\n";
