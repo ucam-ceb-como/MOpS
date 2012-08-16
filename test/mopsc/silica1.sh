5805c47c39eb03f4fcb924cf097f9771b122d926
William Menz <wjm34@cam.ac.uk>
2012-08-16 10:39:42 +0100

    Silica1 test updated to run lightning-fast. Broken into 3 subtests.. inception, interparticle, surface reaction.

--- test/mopsc/silica1.sh
+++ test/mopsc/silica1.sh
@@ -1,85 +1,165 @@
 #!/bin/bash
 
-#  Copyright (C) 2011 William J Menz.
-#
-#   This script tests the Silica model (developed by Shraddha and Markus
-#   Sander) against key metrics used in Preprint 105
-#
-# Licence:
-#    This file is part of "mops".
-#
-#    brush is free software; you can redistribute it and/or
-#    modify it under the terms of the GNU General Public License
-#    as published by the Free Software Foundation; either version 2
-#    of the License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with this program; if not, write to the Free Software
-#    Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-#
-#  Contact:
-#    Prof Markus Kraft
-#    Dept of Chemical Engineering
-#    University of Cambridge
-#    New Museums Site
-#    Pembroke Street
-#    Cambridge
-#    CB2 3RA
-#    UK
-#
-#    Email:       mk306@cam.ac.uk
-#    Website:     http://como.cheng.cam.ac.uk
+# SILICA 1 TEST FOR SWEEP
+# (C) WILLIAM J MENZ (WJM34) 2012
 
-#Path to executable should be supplied as first argument to
-#this script.  Script will fail and return a non-zero value
-#if no executable specified.
-program=$1
+# TESTS KEY PROCESSES FOR THE SILICA PARTICLE MODEL.
+# CASE 1: INCEPTION ONLY
+#   M0 checked
+#
+# CASE 2: INTERPARTICLE REACTION ONLY
+#   Mass, Si:O ratio and water concentration checked
+#
+# CASE 3: SURFACE REACTION ONLY
+#   Dcol, Mass, Si:O ratio and water concentration checked
+#
+# Note that coagulation and sintering are taken care of by the test
+# mops.silica2
 
-if test -z "$program"
-  then
+######################################################################
+# GET ARGUMENTS
+
+# First argument is executable
+if [ -z "$1" ]; then
     echo "No executable supplied to $0"
     exit 255
-fi
-
-# An optional second argument may specify the working directory
-if test -n "$2"
-  then
-    cd $2
-    echo "changed directory to $2"
-fi
-
-cd silica1
-rm -f silica*
-
-$program -p -strang
-simulationResult=$?
-
-if((simulationResult==0)) 
-then
-  echo "Finished simulation"
 else
-  echo "Simulation failed"
-  cd ..
-  exit $simulationResult
-fi
-echo "========================"
-
-
-dos2unix "silica-part.csv"
-perl test-silica.pl
-postprocessResult=$?
-
-if((postprocessResult!=0)) 
-  then
-    cd ..
-    exit $postprocessResult
+    program=$1
 fi
 
-rm -f silica*
-cd ..
-exit 0
+# Second argument is working directory (required)
+cwd=`pwd`
+if [ -z "$2" ]; then
+    echo "No working dir supplied to $0"
+    exit 254
+else
+    wkdir=$2
+fi
+cd $wkdir
+
+######################################################################
+# DEFINE FUNCTIONS
+
+# Checks for failed calculations
+function CheckErr {
+    if [ "$1" -gt "0" ]; then
+        echo "FAILED CALCULATION WITH ERR $1."
+        exit $1
+    fi
+}
+
+# Uses perl to compare two floats.
+function CheckValues {
+perl << EOF
+my \$oldval=$1;
+my \$newval=$2;
+my \$abserr=$3;
+my \$err=0;
+my \$retval=0;
+if(\$oldval != 0) {
+    \$err=abs(\$oldval-\$newval);
+    if (\$err > \$abserr) {
+            print "TEST FAILURE!!! GOT \$newval, wanted \$oldval (err \$err).\n";
+            \$retval=2
+    }
+} 
+exit \$retval
+EOF
+
+CheckErr $?
+}
+
+######################################################################
+# 1: INCEPTION ONLY
+# Obtained for M0 with 10 runs (512 SPs)
+# hash 76f42c72b130ea0dccd1ff6d7abbd37ca7b37686
+true="7.82119E+019"
+absErr="4.76906E+016"
+
+# Run calculation
+echo "Running MOPS for inception case."
+$program -p -strang -rr "mops-incep.inx" -s "sweep-incep.xml" -c "chem.inp" > /dev/null
+CheckErr $?
+
+# Parse CSV to check values.
+csvline1=`tail -1 silica-incep-part.csv`
+line1=(`echo $csvline1 | tr ',' '\n'`)
+m0=${line1[4]}
+CheckValues $true $m0 $absErr
+
+# Clean files
+echo "Inception passes."
+rm silica-incep*
+
+######################################################################
+# 2: INTERPARTICLE ONLY
+# Obtained with 20 runs (128 SPs)
+# hash 76f42c72b130ea0dccd1ff6d7abbd37ca7b37686
+trueM="3.32E-008"
+errM="0.01E-008"
+trueRatio="0.222222"
+errRatio="0.00001"
+trueWater="1.66E-013"
+errWater="0.01E-13"
+
+# Run calculation
+echo "Running MOPS for interparticle case."
+$program -p -strang -rr "mops-intp-sr.inx" -s "sweep-intp.xml" -c "chem-intp.inp" > /dev/null
+CheckErr $?
+
+# Parse CSV to check values.
+csvline1=`tail -1 silica-intp-sr-part.csv`
+line1=(`echo $csvline1 | tr ',' '\n'`)
+mass=${line1[20]}
+CheckValues $trueM $mass $errM
+ratio=${line1[56]}
+CheckValues $trueRatio $ratio $errRatio
+
+csvline1=`tail -1 silica-intp-sr-chem.csv`
+line1=(`echo $csvline1 | tr ',' '\n'`)
+water=${line1[20]}
+CheckValues $trueWater $water $errWater
+
+# Clean files
+echo "Interparticle passes."
+rm silica-intp-sr*
+
+######################################################################
+# 3: SURFACE REACTION ONLY
+# Obtained with 20 runs (128 SPs)
+# hash 76f42c72b130ea0dccd1ff6d7abbd37ca7b37686
+trueDcol="2.05E-008"
+errDcol="0.01E-8"
+trueM="9.88E-007"
+errM="0.01E-007"
+trueRatio="0.327529"
+errRatio="0.00001"
+trueWater="1.22E-011"
+errWater="0.01E-11"
+
+# Run calculation
+echo "Running MOPS for surface reaction case."
+$program -p -strang -rr "mops-intp-sr.inx" -s "sweep-sr.xml" -c "chem-intp.inp" > /dev/null
+CheckErr $?
+
+# Parse CSV to check values.
+csvline1=`tail -1 silica-intp-sr-part.csv`
+line1=(`echo $csvline1 | tr ',' '\n'`)
+dcol=${line1[8]}
+CheckValues $trueDcol $dcol $errDcol
+mass=${line1[20]}
+CheckValues $trueM $mass $errM
+ratio=${line1[56]}
+CheckValues $trueRatio $ratio $errRatio
+
+csvline1=`tail -1 silica-intp-sr-chem.csv`
+line1=(`echo $csvline1 | tr ',' '\n'`)
+water=${line1[20]}
+CheckValues $trueWater $water $errWater
+
+# Clean files
+echo "SR passes."
+rm silica-intp-sr*
+
+echo "All tests passed! :D"
+cd $cwd
