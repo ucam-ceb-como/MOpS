8622172739efed44df902c8d318acd54332ad54c
William Menz <wjm34@cam.ac.uk>
2012-10-11 15:51:42 +0100

    Get the surfvol model to print information about the gstdev of dcol and dpri as well.

--- src/sweepc/include/swp_surfvol_stats.h
+++ src/sweepc/include/swp_surfvol_stats.h
@@ -169,9 +169,16 @@ public:
         );
 
 private:
+    //! Get geometric standard deviation of a list of diameters
+    fvector GetGeometricStdev(
+            const unsigned int num,
+            std::vector<fvector> diams,
+            fvector weights
+            ) const;
+
     // Stats count and indices.
-    static const unsigned int STAT_COUNT = 5;
-    enum StatIndices {iS=0, iPPN=2, iPPD=4};
+    static const unsigned int STAT_COUNT = 7;
+    enum StatIndices {iS=0, iPPN=2, iPPD=4, iCollGStdev=5, iPrimGStdev=6};
 
     // PSL count and indices.
     static const unsigned int PSL_COUNT = 3;
