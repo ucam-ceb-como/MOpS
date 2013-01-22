f729666feff664938bb9723eacb1e790f04f5bca
William Menz <wjm34@cam.ac.uk>
2013-01-22 16:08:29 +0000

    Give Chemkinreader the ability to deal with CRLF lineendings. Finally.

--- src/io/chemkinReader/source/stringFunctions.cpp
+++ src/io/chemkinReader/source/stringFunctions.cpp
@@ -10,27 +10,85 @@
 #include "boost/algorithm/string.hpp"
 #include <string>
 
+namespace {
+
+/*!
+ * New getline function to handle reading of LF or CRLF line endings.
+ * Function taken from StackOverflow with permalink:
+ * http://stackoverflow.com/a/6089413/1021919
+ *
+ * @param is    Input filestream
+ * @param t     Line to get
+ * @return
+ */
+std::istream& safeGetline(std::istream& is, std::string& t)
+{
+    t.clear();
+
+    std::istream::sentry se(is, true);
+    std::streambuf* sb = is.rdbuf();
+
+    for(;;) {
+        int c = sb->sbumpc();
+        switch (c) {
+        case '\n':
+            return is;
+        case '\r':
+            if(sb->sgetc() == '\n')
+                sb->sbumpc();
+            return is;
+        case EOF:
+            // Also handle the case when the last line has no line ending
+            if(t.empty())
+                is.setstate(std::ios::eofbit);
+            return is;
+        default:
+            t += (char)c;
+        }
+    }
+}
+
+}
+
+
 using namespace std;
 using namespace boost;
 
-string
-IO::fileToString(const string& fileName)
+/*!
+ * Read a file and convert it to a string, INCLUDING the line
+ * ending characters. Also convert all to caps. Use safe getline
+ * to permit parsing of CRLF lines endings.
+ *
+ * @param fileName  Name of file to read
+ * @return          File as a string
+ */
+string IO::fileToString(const string& fileName)
 {
     ifstream fin(fileName.c_str(), ios::in);
 
-    string fileInString((istreambuf_iterator<char>(fin)),
-                         istreambuf_iterator<char>());
-
-    return convertToCaps(fileInString);
+    string fs;
+    string line;
+    while (::safeGetline(fin, line)) {
+        fs.append(convertToCaps(line));
+        fs.append("\n");
+    }
+    return convertToCaps(fs);
 }
 
-vector<string>
-IO::fileToStrings(const string fileName)
+/*!
+ * Read a file and convert it to a vector of strings, with each line a new
+ * entry in the vector. Use safe getline to permit parsing of CRLF line
+ * endings.
+ *
+ * @param fileName  Name of file to read
+ * @return          File as a vector of strings
+ */
+vector<string> IO::fileToStrings(const string fileName)
 {
     vector<string> lines;
     ifstream fin(fileName.c_str(), ios::in);
     string line;
-    while (getline(fin, line)) {
+    while (::safeGetline(fin, line)) {
         lines.push_back(convertToCaps(line));
     }
     return lines;
